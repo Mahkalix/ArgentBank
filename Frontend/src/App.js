c05dc2b7bf0ec04e59275ed0f6e7f47284e58313
Maxence Badin Léger <maxencelger@gmail.com>
2023-07-26 14:14:14 +0200

    Initialisation React App

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import "../src/style/App.css";
+import Route from "../src/router/route";
+
+function App() {
+  return (
+    <div className="App">
+      <Route />
+    </div>
+  );
+}
+
+export default App;
